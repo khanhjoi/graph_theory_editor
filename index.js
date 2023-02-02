00156df8f7b4e872979ac1eb4e8700b688c32f1c
khanhjoi <khanhb2003835@student.ctu.edu.vn>
2023-02-02 18:12:24 +0700

    add nodes

--- index.js
+++ index.js
@@ -1,42 +1,86 @@
+var cy =   cytoscape({
+  container: document.getElementById('cy'),
+  elements: {
+    nodes: [
+      {
+        data: { id: '1' }
+      },
 
-
-var cy = cytoscape({
-    container: document.getElementById('cy'),
-
-    elements: [ // list of graph elements to start with
-        { // node a
-        data: { id: 'a' }
-        },
-        { // node b
-        data: { id: 'b' }
-        },
-        { // edge ab
-        data: { id: 'ab', source: 'a', target: 'b' }
-        }
+      {
+        data: { id: '2' }
+      },
     ],
-    style: [ // the stylesheet for the graph
+    edges: [
+      {
+        data: { id: 'ab', source: '1', target: '2' }
+      },
+     
+    ]
+    },
+
+  layout: {
+      name: 'grid',
+      rows: 1
+  },
+
+// so we can see the ids
+  style: [
     {
       selector: 'node',
       style: {
-        'background-color': '#666',
         'label': 'data(id)'
-      }
-    },
-
-    {
-      selector: 'edge',
-      style: {
-        'width': 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle',
-        'curve-style': 'bezier'
       }
     }
   ],
-
-  layout: {
-    name: 'grid',
-    rows: 1
-  }
+  panningEnabled: false,
 });
+
+
+var numberNodes = 2;
+
+
+// chon va xoa 
+cy.on('click', function(e){
+  // bien su kien
+  var clicked = e.target;
+
+  let btnAdd = document.getElementById('add');
+  if(btnAdd.checked) {
+    numberNodes++;
+    cy.add([
+      { group: 'nodes', data: { id: `${numberNodes}` }, position: { x: 300, y: 300 } },
+    ]);
+    
+    let btnAddEdg = document.getElementById('addEdge');
+    if(btnAddEdg.value) {
+      cy.add([
+        { group: 'edges', data: { id: `${btnAddEdg.value}`, source: '3', target: '4' } }
+      ]);
+    }
+  }
+  
+  console.log();
+
+
+// =============== xóa đỉnh =================
+  // cờ
+  var flag = false;
+  // kiểm tra có phải là đỉnh hoặt cung => xóa
+  if(clicked._private.group === "nodes" || clicked._private.group === "edges" ){
+    flag = true
+  }else {
+    // reset lại cờ
+    flag = false;
+  }
+  
+  const btnDlt = document.getElementById('delete');
+  btnDlt.onclick = function() {
+    if(flag){
+      cy.remove(clicked);
+    }
+  }
+// =========================================
+});
+
+
+
